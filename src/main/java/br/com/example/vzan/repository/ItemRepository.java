ecf425883048a24fc12ed080ed89587c66fa4fa4
vzanbr <128941678+vzanbr@users.noreply.github.com>
2023-09-08 22:19:59 -0300

    Feature: Implementacao de seguranca para usuarios(Part 3).

--- src/main/java/br/com/example/vzan/repository/ItemRepository.java
+++ src/main/java/br/com/example/vzan/repository/ItemRepository.java
@@ -1,6 +1,6 @@
 package br.com.example.vzan.repository;
 
-import br.com.example.vzan.item.Item;
+import br.com.example.vzan.domain.item.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ItemRepository extends JpaRepository<Item, String> {
--- src/main/java/br/com/example/vzan/repository/UserRepository.java
+++ src/main/java/br/com/example/vzan/repository/UserRepository.java
@@ -1,9 +1,9 @@
 package br.com.example.vzan.repository;
 
-import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import br.com.example.vzan.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 
-public interface UserRepository extends JpaRepository<SecurityProperties.User, String> {
+public interface UserRepository extends JpaRepository<User, String> {
     UserDetails findByLogin(String login);
 }
